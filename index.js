e67a6db1db38170fda73ec94968b2865e21dd0b2
Denzel <jdenzelmab@gmail.com>
2023-03-13 21:07:18 -0400

    Completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let multiply;
+let num1 = 2;
+let num2 = 31;
+multiply = num1*num2;
+
+let random =  Math.floor(Math.random()+ 1);
+
+let mod;
+let num3 = 9;
+let num4 = 5;
+mod = num3 % num4;
+
+let max = Math.max(20);
